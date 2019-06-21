e2859a0bd550108326aa4c31c06c1f9bc624bd31
ilakkiyag <ilakkiya@nurture.co.in>
2019-06-21 11:51:19 +0530

    Ilakkiya-21-06-2019(DB configuration changes)
    Product new Db server added 192.168.168.6 Postgres

--- FINWIZ_WS/routes/database/pgdbconnect_hrm.js
+++ FINWIZ_WS/routes/database/pgdbconnect_hrm.js
@@ -1,10 +1,20 @@
 var pg=require('pg');
 
+// var config = {
+// user:'postgres',
+// database:'PACS_HRM',//first connection folder
+// password:'postgres',
+// host:'192.168.168.4',
+// port:5432,
+// max:10000,
+// idleTimeoutMillis: 30000,
+// };
+
 var config = {
 user:'postgres',
-database:'PACS_HRM',//first connection folder
-password:'postgres',
-host:'192.168.168.4',
+database:'postgres',//first connection folder
+password:'nurture@123',
+host:'192.168.168.6',
 port:5432,
 max:10000,
 idleTimeoutMillis: 30000,
